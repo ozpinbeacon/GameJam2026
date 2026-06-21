1697dbba3066d467baf7ca313c7b0bd22f3bf02b
ozpinbeacon <liamthesquirrel@gmail.com>
2026-06-22 09:14:12 +1000

    chore: learning lighting, accelerated movement

--- player.gd
+++ player.gd
@@ -2,32 +2,49 @@ extends CharacterBody3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var air_resistance = 1
-var speed = 2
-var jump_speed = 5
+@export var playerSpeed = 5
+@export var playerAcceleration = 5
+@export var cameraSensitivity = 0.25
+@export var cameraAcceleration = 2
+@export var jump_speed = 5
 var mouse_sensitivity = 0.002
 
-func _physics_process(delta):
-	velocity.y += -gravity * delta
-	if is_on_floor() and Input.is_action_pressed("sprint"):
-		speed = 5
-	if is_on_floor() and not Input.is_action_pressed("sprint"):
-		speed = 2
-	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
-	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
-	
-	if not is_on_floor():
-		velocity.x = movement_dir.x * speed - (air_resistance * delta)
-		velocity.z = movement_dir.z * speed - (air_resistance * delta)
-	else:
-		velocity.x = movement_dir.x * speed
-		velocity.z = movement_dir.z * speed
-	
-	move_and_slide()
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = jump_speed
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+@onready var hand = $Hand
+@onready var flashlight = $Hand/SpotLight3D
 
+var direction = Vector3.ZERO
+var head_y_axis = 0.0
+var camera_x_axis = 0.0
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
 func _input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(-event.relative.x * mouse_sensitivity)
-		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
-		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		head_y_axis += event.relative.x * cameraSensitivity
+		camera_x_axis += event.relative.y * cameraSensitivity
+		
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event.is_action_pressed("click"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	direction = Input.get_axis("move_left", "move_right") * head.basis.x + Input.get_axis("move_forward", "move_backwards") * head.basis.z
+	velocity = velocity.lerp(direction * playerSpeed + velocity.y * Vector3.UP, playerAcceleration * delta)
+	
+	head.rotation.y = lerp(head.rotation.y, -deg_to_rad(head_y_axis), cameraAcceleration * delta)
+	camera.rotation.x = lerp(camera.rotation.x, -deg_to_rad(camera_x_axis), cameraAcceleration * delta)
+	
+	hand.rotation.y = -deg_to_rad(head_y_axis)
+	hand.rotation.x = -deg_to_rad(camera_x_axis)
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y += jump_speed
+	else:
+		velocity.y -= gravity * delta
+		
+	move_and_slide()
